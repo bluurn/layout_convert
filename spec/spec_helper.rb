65c4ebcf6bcd295866d17468007712a54d9ee899
Vovan <bluurn@gmail.com>
2014-07-04 15:17:45 +0400

    try setting up CC

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 require "codeclimate-test-reporter"
+ENV['CODECLIMATE_REPO_TOKEN'] = "51960221e8151ad9b40733ea47d536f89f5da3f418b350297c06f23e45f11e14"
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+]
 CodeClimate::TestReporter.start
